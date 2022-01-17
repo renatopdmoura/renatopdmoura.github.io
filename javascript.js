d9b27c29ae2733609450d3298ebfef3fca168099
renatopdmoura <renato.dmoura@hotmail.com>
2022-01-16 23:22:15 -0300

    Update files

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
+// Escrito por Renato Moura em 16 de Janeiro de 2021
 
-item_1 ="Um gerador de programa de sombreamento de fabrica. É util para definir objetos de programas que serão compartilhados por diferentes instâncias.";
-item_2 ="Um gerador de programa de sombreamento para situaçãos em que se faz necessário um sombreador de geometria.";
-item_3 ="Um gerador de programa de sombreamento.";
-
+function catch_text(id){
+	index = "text-" + id;
+	return document.getElementById('text-' + id).innerHTML;
+}
 
 function clear_text(){
 	var item_count = document.getElementById("func-list").childElementCount;
-	for(var i = 1; i < item_count + 1; i++){
+	for(var i = 0; i < item_count + 1; i++){
 		document.getElementById(i).innerHTML = "";
 	}
 }
@@ -15,13 +16,13 @@ function switch_article(id){
 	clear_text();
 	switch(id){		
 		case 1:
-			document.getElementById(id).innerHTML = item_1;
+			document.getElementById(id).innerHTML = catch_text(id);
 			break;
 		case 2:
-			document.getElementById(id).innerHTML = item_2;
+			document.getElementById(id).innerHTML = catch_text(id);
 			break;
 		case 3:
-			document.getElementById(id).innerHTML = item_3;
+			document.getElementById(id).innerHTML = catch_text(id);
 			break;
 	}
 }
