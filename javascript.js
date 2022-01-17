befcfe6660fa432997451ea82e05c5f6247257d0
renatopdmoura <renato.dmoura@hotmail.com>
2022-01-17 15:50:05 -0300

    Update files

--- javascript.js
+++ javascript.js
@@ -6,23 +6,31 @@ function catch_text(id){
 }
 
 function clear_text(){
+	let w = window.innerWidth;
 	var item_count = document.getElementById("func-list").childElementCount;
 	for(var i = 1; i < item_count + 1; i++){
-		document.getElementById('mobile-view-' + i).innerHTML = "";
+		if(w >= 1366)
+			document.getElementById('desktop-view-' + i).innerHTML = "";
+		else
+			document.getElementById('mobile-view-' + i).innerHTML = "";
 	}
+	if(w >= 1366)
+		return 'desktop-view-';
+	else
+		return 'mobile-view-';
 }
 
 function switch_article(id){
-	clear_text();
+	let label_id = clear_text() + id;
 	switch(id){		
 		case 1:
-			document.getElementById('mobile-view-' + id).innerHTML = catch_text(id);
+			document.getElementById(label_id).innerHTML = catch_text(id);
 			break;
 		case 2:
-			document.getElementById('mobile-view-' + id).innerHTML = catch_text(id);
+			document.getElementById(label_id).innerHTML = catch_text(id);
 			break;
 		case 3:
-			document.getElementById('mobile-view-' + id).innerHTML = catch_text(id);
+			document.getElementById(label_id).innerHTML = catch_text(id);
 			break;
 	}
 }
