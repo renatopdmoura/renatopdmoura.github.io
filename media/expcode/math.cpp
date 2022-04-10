a3a32ad036a9427bfcbb45403b58a8363c2d1951
renatopdmoura <renato.dmoura@hotmail.com>
2022-04-10 18:14:56 -0300

    Update files

---
+++ media/expcode/math.cpp
@@ -0,0 +1,17 @@
+#include "math.hpp"
+
+int sum(int a, int b){
+	return a + b;
+}
+
+int subtract(int a, int b){
+	return a - b;
+}
+
+int multiply(int a, int b){
+	return a * b;
+}
+
+int division(int a, int b){
+	return a / b;
+}
---
+++ media/expcode/math.hpp
@@ -0,0 +1,9 @@
+#ifndef MATH_HPP
+#define MATH_HPP
+
+int sum(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int division(int a, int b);
+
+#endif
